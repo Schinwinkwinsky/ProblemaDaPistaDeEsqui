8b76e0f082761d127807c689fa30ece7570e043e
Sérgio Damasceno de Matos <sergio.damasceno.matos@outlook.com>
2017-01-11 11:17:09 -0300

    Elevador atualizado!

--- src/main/Elevador.java
+++ src/main/Elevador.java
@@ -5,6 +5,7 @@
  */
 package main;
 
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,8 +18,6 @@ public class Elevador implements Runnable {
     Filas filas;
     int vazio = 4;
     int contadorElevador = 1;
-    boolean leftTriple = false;
-    boolean rightTriple = false;
 
     public Elevador(Filas f) {
         filas = f;
@@ -35,69 +34,68 @@ public class Elevador implements Runnable {
     @Override
     public synchronized void run() {
         while (true) {
-            try {
-                if (filas.getLeftTriple().size() > 2
-                        && vazio > 2) {
-                    for (int i = 2; i >= 0; i--) {
-                        filas.getLeftTriple().remove();
-                        vazio--;
-                    }
-                    leftTriple = true;
-                }
+            boolean leftTriple = false;
+            boolean rightTriple = false;
 
-                if (filas.getRightTriple().size() > 2
-                        && vazio > 2) {
-                    for (int i = 2; i >= 0; i--) {
-                        filas.getRightTriple().remove();
-                        vazio--;
-                    }
-                    rightTriple = true;
+            if (filas.getLeftTriple().size() > 2
+                    && vazio > 2) {
+                for (int i = 2; i >= 0; i--) {
+                    filas.getLeftTriple().remove();
+                    vazio--;
                 }
-
-                //Caso não tenha embarcado ninguém das filas anteriores.
-                if (!leftTriple && !rightTriple) {
-                    while(vazio > 0){
-                        if (filas.getLeftSingle().size() > 0) {
-                            filas.getLeftSingle().remove();
-                            vazio--;
-                        }
-                        
-                        if(filas.getRightSingle().size() > 0){
-                            filas.getRightSingle().remove();
-                            vazio--;
-                        }
-                    }
-                    
-                    while (filas.getLeftSingle().size() > 0
-                            && vazio > 0) {
-                        filas.getLeftSingle().remove();
-                        vazio--;
-                    }
-
-                    while (filas.getRightSingle().size() > 0
-                            && vazio > 0) {
-                        filas.getRightSingle().remove();
-                        vazio--;
-                    }
-                }else{
-                    if (leftTriple
-                            && filas.getRightSingle().size() > 0) {
-                        filas.getRightSingle().remove();
-                        vazio--;
-                    }
-                    
-                    if (rightTriple
-                            && filas.getLeftSingle().size() > 0) {
-                        filas.getLeftSingle().remove();
-                        vazio--;
-                    }
-                }
-                
                 leftTriple = true;
+            }
+
+            if (filas.getRightTriple().size() > 2
+                    && vazio > 2) {
+                for (int i = 2; i >= 0; i--) {
+                    filas.getRightTriple().remove();
+                    vazio--;
+                }
                 rightTriple = true;
+            }
 
-                imprimirElevador();
+            //Caso não tenha embarcado ninguém das filas anteriores.
+            if (!leftTriple && !rightTriple) {
 
+                //Utilizada para alternar nas filas.
+                boolean chave = true;
+
+                while (vazio > 0
+                        && (filas.getLeftSingle().size() > 0
+                        || filas.getRightSingle().size() > 0)) {
+
+                    if (filas.getLeftSingle().size() > 0
+                            && chave) {
+                        filas.getLeftSingle().remove();
+                        vazio--;
+                        chave = false;
+                    }
+
+                    if (filas.getRightSingle().size() > 0
+                            && !chave) {
+                        filas.getRightSingle().remove();
+                        vazio--;
+                        chave = true;
+                    }
+                }
+            } else {
+                if (leftTriple
+                        && filas.getRightSingle().size() > 0) {
+                    filas.getRightSingle().remove();
+                    vazio--;
+                }
+
+                if (rightTriple
+                        && filas.getLeftSingle().size() > 0) {
+                    filas.getLeftSingle().remove();
+                    vazio--;
+                }
+            }
+
+            imprimirElevador();
+
+            try {
                 this.wait(4000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Elevador.class.getName()).log(Level.SEVERE, null, ex);
